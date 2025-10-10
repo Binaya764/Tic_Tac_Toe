a505470303b69994e6b921ffff7b5e29101e3d09
Binaya764 <binaya0724@student.ku.edu.np>
2025-10-10 09:18:30 +0545

    board created

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ class Tic_Toe:
     def __init__(self):
         self.board = [ " " for _ in range (9)]
         print(self.board)
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('|'+'|'.join(row)+ '|')
         
 T= Tic_Toe()
+create= T.print_board()
